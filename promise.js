7c1c8e10eef70c430ffe9467b943bd0d6c9a33ea
mfahminh <muhammadel142@gmail.com>
2022-09-18 20:05:08 +0700

    membuat fungsi promiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,30 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const promiseOutput = async (emosi) =>{
+  try {
+    const getPromiseTheaterIXX = await promiseTheaterIXX();
+    const getPromiseTheaterVGC = await promiseTheaterVGC();
+    
+    const mergedTheater = getPromiseTheaterIXX.concat(getPromiseTheaterVGC);
+    let hasil = 0;
+    Object.values(mergedTheater).forEach(val => {
+    
+      const result = [val.hasil].filter(i => i === (emosi));
+      const count = (result).length;
+      hasil += count ? 1 : 0;
+    });
+
+    console.log(hasil);
+
+  }catch (err) {
+    console.log(err);
+  }
+};
+
+promiseOutput('marah');
+//promiseOutput('tidak marah');
 
 module.exports = {
   promiseOutput,
